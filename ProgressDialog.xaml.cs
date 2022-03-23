8e5017018f0066b221db40183f1cf81cbe766a70
DismissedLight <1686188646@qq.com>
2022-03-23 22:59:29 +0800

    immutable dialog

---
+++ ProgressDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using DGP.Genshin.Control.Infrastructure.Observable;
+using ModernWpf.Controls;
+
+namespace Genshin.Launcher.Plus.SE.Plugin
+{
+    /// <summary>
+    /// ProgressDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProgressDialog : ObservableContentDialog
+    {
+        public ProgressDialog()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsCloseAllowed { get; set; }
+
+        private void DialogClosing(ContentDialog sender, ContentDialogClosingEventArgs args)
+        {
+            if (IsCloseAllowed == false)
+            {
+                args.Cancel = true;
+            }
+        }
+    }
+}
